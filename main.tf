ae5171308e201ca0ab6460d3b0fb4ac3e626fcda
James Holland <jamoiholland@gmail.com>
2020-12-03 16:16:07 +0000

    VAriables

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
   version = "2.33.0"
-
   region = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 provider "random" {
--- variables.tf
+++ variables.tf
@@ -1,11 +1,17 @@
+// AWS Variables
 variable "aws_region" {
   type    = string
   default = "eu-west-2"
 }
 
-variable "panos_version" {
+variable "aws_access_key" {
   type    = string
-  default = "9.1.0"
+  default = ""
+}
+
+variable "aws_secret_key" {
+  type    = string
+  default = ""
 }
 
 variable "db_table_name" {
@@ -21,4 +27,11 @@ variable "db_read_capacity" {
 variable "db_write_capacity" {
   type    = number
   default = 1
+}
+
+// PAN-OS Variables
+
+variable "panos_version" {
+  type    = string
+  default = "9.1.0"
 }
