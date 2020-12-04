cc3d9ac3c119d229a08f6921d73c53d19083f871
James Holland <jamoiholland@gmail.com>
2020-12-04 20:03:51 +0000

    More quoted expressions!

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,35 @@
 output "vpc-VPC-ID" {
-    value = "${aws_vpc.pavm-vpc.id}"
+    value = aws_vpc.pavm-vpc.id
 }
 
 output "subnet-Management-Subnet-ID" {
-    value = "${aws_subnet.mgmt-subnet.id}"
+    value = aws_subnet.mgmt-subnet.id
 }
 
 output "vpc-Default-Security-Group-ID" {
-    value = "${aws_security_group.default-security-gp.id}"
+    value = aws_security_group.default-security-gp.id
 }
 
 output "general-Instance-ID" {
-    value = "${aws_instance.pavm.id}"
+    value = aws_instance.pavm.id
 }
 
 output "ip-Management-Public-IP" {
-    value = "${aws_instance.pavm.public_ip}"
+    value = aws_instance.pavm.public_ip
 }
 
 output "ip-Management-Private-IP" {
-    value = "${aws_instance.pavm.private_ip}"
+    value = aws_instance.pavm.private_ip
 }
 
 output "ip-Untrust-Public-IP" {
-    value = "${aws_eip.untrust_eip.public_ip}"
+    value = aws_eip.untrust_eip.public_ip
 }
 
 output "ip-Untrust-Private-IP" {
-    value = "${aws_eip.untrust_eip.private_ip}"
+    value = aws_eip.untrust_eip.private_ip
 }
 
 output "ip-Trust-Private-IP" {
-    value = "${aws_network_interface.trust_eni.private_ips}"
+    value = aws_network_interface.trust_eni.private_ips
 }
